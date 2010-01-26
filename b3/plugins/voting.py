18cb2e9383f943878f43cdc1a06a5c1584d42a27
Ismael <ismaelgfk@gmail.com>
2010-01-26 16:34:12 -0200

    Reindented, now restarts on game_warmup event

--- b3/plugins/voting.py
+++ b3/plugins/voting.py
@@ -17,6 +17,8 @@
 #  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
 # CHANGELOG
+# 24/01/10
+# Reset on gamewarmup
 # 25/07/09
 # Added !votemap
 # Revamped the system, more modular now.
@@ -29,309 +31,314 @@
 # Added delays between failed votes
 # Initial version
 
-__version__ = '1.5'
+__version__ = '1.5.1'
 __author__  = 'Ismael'
 
 import b3
-import b3.plugin
-import b3.cron
 from b3 import clients
+import b3.cron
+import b3.plugin
 
 class VotingPlugin(b3.plugin.Plugin):
-	_adminPlugin = None
-	_currentVote = None
+    _adminPlugin = None
+    _currentVote = None
 
-	_caller = None
+    _caller = None
 
-	_in_progress = False
-	_yes = 0
-	_no = 0
-	_vetoed = 0
-	_times = 0
-	_vote_times = 3
-	_vote_interval = 0
+    _in_progress = False
+    _yes = 0
+    _no = 0
+    _vetoed = 0
+    _times = 0
+    _vote_times = 3
+    _vote_interval = 0
 	
-	_votes = {}
+    _votes = {}
 	
-	def startup(self):
-		"""\
+    def startup(self):
+        """\
 		Initialize plugin settings
 		"""
 
-		# get the plugin so we can register commands
-		self._adminPlugin = self.console.getPlugin('admin')
-		if not self._adminPlugin:
-			# something is wrong, can't start without admin plugin
-			self.error('Could not find admin plugin')
-			return False
+        # get the plugin so we can register commands
+        self._adminPlugin = self.console.getPlugin('admin')
+        if not self._adminPlugin:
+            # something is wrong, can't start without admin plugin
+            self.error('Could not find admin plugin')
+            return False
 		
-		self._vote_times = self.config.getint('settings', 'vote_times')
-		self._vote_interval = self.config.getint('settings', 'vote_interval')
-		minLevel_vote = self.config.getint('settings', 'min_level_vote')
-		modLevel = self._adminPlugin.config.getint("settings","admins_level")  
+        self._vote_times = self.config.getint('settings', 'vote_times')
+        self._vote_interval = self.config.getint('settings', 'vote_interval')
+        minLevel_vote = self.config.getint('settings', 'min_level_vote')
+        modLevel = self._adminPlugin.config.getint("settings", "admins_level")
 		
-		self._adminPlugin.registerCommand(self, 'voteyes', minLevel_vote,  self.cmd_voteyes,  'vy')
-		self._adminPlugin.registerCommand(self, 'voteno', minLevel_vote, self.cmd_voteno,  'vn')
-		self._adminPlugin.registerCommand(self, 'voteveto', modLevel, self.cmd_veto,  'vveto')
+        self._adminPlugin.registerCommand(self, 'voteyes', minLevel_vote, self.cmd_voteyes, 'vy')
+        self._adminPlugin.registerCommand(self, 'voteno', minLevel_vote, self.cmd_voteno, 'vn')
+        self._adminPlugin.registerCommand(self, 'voteveto', modLevel, self.cmd_veto, 'vveto')
 
-		minLevel_kick = self.config.getint('votekick', 'min_level_kick')
-		self._adminPlugin.registerCommand(self, 'votekick', minLevel_kick, self.cmd_votekick,  'vk')
+        minLevel_kick = self.config.getint('votekick', 'min_level_kick')
+        self._adminPlugin.registerCommand(self, 'votekick', minLevel_kick, self.cmd_votekick, 'vk')
 
-		self._votes["kick"] = KickVote()
-		self._votes["kick"].startup(self._adminPlugin,  self.console,  self.config)
+        self._votes["kick"] = KickVote()
+        self._votes["kick"].startup(self._adminPlugin, self.console, self.config)
 
-		minLevel_map = self.config.getint('votemap', 'min_level_map')
-		self._adminPlugin.registerCommand(self, 'votemap', minLevel_map, self.cmd_votemap,  'vm')
-		self._adminPlugin.registerCommand(self, 'maplist', 0, self.cmd_maplist,  'mapl')
-		self._votes["map"] = MapVote()
-		self._votes["map"].startup(self._adminPlugin,  self.console,  self.config)
+        minLevel_map = self.config.getint('votemap', 'min_level_map')
+        self._adminPlugin.registerCommand(self, 'votemap', minLevel_map, self.cmd_votemap, 'vm')
+        self._adminPlugin.registerCommand(self, 'maplist', 0, self.cmd_maplist, 'mapl')
+        self._votes["map"] = MapVote()
+        self._votes["map"].startup(self._adminPlugin, self.console, self.config)
 
+        #To avoid hanging up
+        self.registerEvent(b3.events.EVT_GAME_WARMUP)
 
-	def cmd_maplist(self,  data,  client,  cmd=None):
-		client.message("Maps available: " + ", ".join(self._votes["map"]._mapList))
+    def onEvent(self, evt):
+        self._in_progress = False
+
+    def cmd_maplist(self, data, client, cmd=None):
+        client.message("Maps available: " + ", ".join(self._votes["map"]._mapList))
 	
-	def pre_vote(self,  client):
-		if self._in_progress:
-			client.message("A vote is already in progress, wait until it finishes")
-			return False
+    def pre_vote(self, client):
+        if self._in_progress:
+            client.message("A vote is already in progress, wait until it finishes")
+            return False
 		
-		if client.var(self,  'holding_vote').value:
-			client.message("You have to wait between failed votes!")
-			return False
-		return True
+        if client.var(self, 'holding_vote').value:
+            client.message("You have to wait between failed votes!")
+            return False
+        return True
 
 
-	def cmd_votekick(self, data, client, cmd=None):
-		"""\
+    def cmd_votekick(self, data, client, cmd=None):
+        """\
 		<name> <motive> - call a votekick on that player for that motive
 		"""
-		if not self.pre_vote(client):
-			return False
+        if not self.pre_vote(client):
+            return False
 		
-		self._currentVote = self._votes["kick"]
+        self._currentVote = self._votes["kick"]
 		
-		if not self._currentVote.start_vote(data,  client):
-			return False
+        if not self._currentVote.start_vote(data, client):
+            return False
 		
-		self.go_vote(client)
+        self.go_vote(client)
 
-	def cmd_votemap(self, data, client, cmd=None):
-		"""\
+    def cmd_votemap(self, data, client, cmd=None):
+        """\
 		<map> - call a votemap for that map
 		"""
-		if not self.pre_vote(client):
-			return False
+        if not self.pre_vote(client):
+            return False
 		
-		self._currentVote = self._votes["map"]
+        self._currentVote = self._votes["map"]
 
-		if not self._currentVote.start_vote(data,  client):
-			return False
+        if not self._currentVote.start_vote(data, client):
+            return False
 		
-		self.go_vote(client)
+        self.go_vote(client)
 
-	def go_vote(self,  client):
-		self._caller = client
-		self._in_progress = True
-		self._times = self._vote_times
-		self._no = 0
-		self._vetoed = 0
+    def go_vote(self, client):
+        self._caller = client
+        self._in_progress = True
+        self._times = self._vote_times
+        self._no = 0
+        self._vetoed = 0
 		
-		self._yes = 1
-		client.var(self,  'voted').value = True #The caller of the vote votes yes by default
+        self._yes = 1
+        client.var(self, 'voted').value = True #The caller of the vote votes yes by default
 		
-		reason = self._currentVote.vote_reason()
-		self.console.say("Calling a vote " + reason)
-		self.console.say("Type ^1!vy ^7to vote ^1yes^7, ^2!vn ^7to vote ^2no")
-		self.console.cron + b3.cron.OneTimeCronTab(self.update_vote,  "*/1")
+        reason = self._currentVote.vote_reason()
+        self.console.say("Calling a vote " + reason)
+        self.console.say("Type ^1!vy ^7to vote ^1yes^7, ^2!vn ^7to vote ^2no")
+        self.console.cron + b3.cron.OneTimeCronTab(self.update_vote, "*/1")
 	
 
-	def cmd_veto(self, data, client, cmd=None):
-		self._vetoed = 1
+    def cmd_veto(self, data, client, cmd=None):
+        self._vetoed = 1
 
-	def update_vote(self):
-		if not self._vetoed:
-			reason = self._currentVote.vote_reason()
-			self.console.say("[%d/%d] Voting " % (self._vote_times - self._times + 1,  self._vote_times) + reason)
-			self.console.say("Type ^1!vy ^7to vote ^1yes^7, ^2!vn ^7to vote ^2no")
-			self.console.say("^1Yes: %s^7, ^2No: %s" %(self._yes,  self._no))
-			self._times -= 1
-			if self._times > 0:
-				self.console.cron + b3.cron.OneTimeCronTab(self.update_vote,  "*/1")
-			else:
-				self.console.cron + b3.cron.OneTimeCronTab(self.end_vote,  "*/1")
-		else:
-			self.console.say("Vote ^1vetoed!")
-			self._in_progress = False
-			self._currentVote = None
+    def update_vote(self):
+        if not self._vetoed:
+            reason = self._currentVote.vote_reason()
+            self.console.say("[%d/%d] Voting " % (self._vote_times - self._times + 1, self._vote_times) + reason)
+            self.console.say("Type ^1!vy ^7to vote ^1yes^7, ^2!vn ^7to vote ^2no")
+            self.console.say("^1Yes: %s^7, ^2No: %s" % (self._yes, self._no))
+            self._times -= 1
+            if self._times > 0:
+                self.console.cron + b3.cron.OneTimeCronTab(self.update_vote, "*/1")
+            else:
+                self.console.cron + b3.cron.OneTimeCronTab(self.end_vote, "*/1")
+        else:
+            self.console.say("Vote ^1vetoed!")
+            self._in_progress = False
+            self._currentVote = None
 	
-	def end_vote(self):
-		self.console.say("Vote ended")
-		self.console.say("^1Yes: %s^7, ^2No: %s" %(self._yes,  self._no))
-		if self._yes > self._no:
-			self._currentVote.end_vote_yes(self._yes,  self._no)
-		else:
-			self._currentVote.end_vote_no(self._yes,  self._no)
-			#The vote failed, the caller can't call another vote for a while
-			self._caller.var(self,  'holding_vote').value = True
-			temp = self._caller
-			def let_caller_vote():
-				self.debug("clearing %s" % temp.exactName)
-				temp.var(self,  'holding_vote').value = False
+    def end_vote(self):
+        self.console.say("Vote ended")
+        self.console.say("^1Yes: %s^7, ^2No: %s" % (self._yes, self._no))
+        if self._yes > self._no:
+            self._currentVote.end_vote_yes(self._yes, self._no)
+        else:
+            self._currentVote.end_vote_no(self._yes, self._no)
+            #The vote failed, the caller can't call another vote for a while
+            self._caller.var(self, 'holding_vote').value = True
+            temp = self._caller
+            def let_caller_vote():
+                self.debug("clearing %s" % temp.exactName)
+                temp.var(self, 'holding_vote').value = False
 			
-			self.console.cron + b3.cron.OneTimeCronTab(let_caller_vote,  0, "*/%s" %self._vote_interval)
+            self.console.cron + b3.cron.OneTimeCronTab(let_caller_vote, 0, "*/%s" % self._vote_interval)
 		
-		self._in_progress = False
-		self._currentVote = None
+        self._in_progress = False
+        self._currentVote = None
 	
-		for c in self.console.clients.getList():
-			c.var(self,  'voted').value = False
+        for c in self.console.clients.getList():
+            c.var(self, 'voted').value = False
 
-	def cmd_voteyes(self, data, client, cmd=None):
-		if self.vote(client,  cmd):
-			self._yes += 1
-			cmd.sayLoudOrPM(client,  "Voted ^1YES")
+    def cmd_voteyes(self, data, client, cmd=None):
+        if self.vote(client, cmd):
+            self._yes += 1
+            cmd.sayLoudOrPM(client, "Voted ^1YES")
 
-	def cmd_voteno(self, data, client, cmd=None):
-		if self.vote(client,  cmd):
-			self._no += 1
-			cmd.sayLoudOrPM(client,  "Voted ^2NO")
+    def cmd_voteno(self, data, client, cmd=None):
+        if self.vote(client, cmd):
+            self._no += 1
+            cmd.sayLoudOrPM(client, "Voted ^2NO")
 	
-	def vote(self,  client,  cmd):
-		if self._in_progress:
-			if not client.var(self,  'voted').value:
-				client.var(self,  'voted').value = True
-				return True
-			else:
-				cmd.sayLoudOrPM(client,  "You already voted!")
-		else:
-			cmd.sayLoudOrPM(client,  "No vote in progress")
-		return False
+    def vote(self, client, cmd):
+        if self._in_progress:
+            if not client.var(self, 'voted').value:
+                client.var(self, 'voted').value = True
+                return True
+            else:
+                cmd.sayLoudOrPM(client, "You already voted!")
+        else:
+            cmd.sayLoudOrPM(client, "No vote in progress")
+        return False
 
 class KickVote(object):
-	_adminPlugin = None
-	console = None
-	config = None
+    _adminPlugin = None
+    console = None
+    config = None
 	
-	_victim = None
-	_caller = None
-	_reason = None
+    _victim = None
+    _caller = None
+    _reason = None
 
-	_modLevel = 20
+    _modLevel = 20
 
-	_tempban_interval = 0
-	_tempban_percent	= 0
-	_tempban_minvotes = 0
+    _tempban_interval = 0
+    _tempban_percent	= 0
+    _tempban_minvotes = 0
 
-	def startup(self,  adminPlugin,  console,  config):
-		"""\
+    def startup(self, adminPlugin, console, config):
+        """\
 		Initialize plugin settings
 		"""
 
-		self._adminPlugin = adminPlugin
-		self.console = console
-		self.config = config
+        self._adminPlugin = adminPlugin
+        self.console = console
+        self.config = config
 
-		self._modLevel = self._adminPlugin.config.getint("settings","admins_level")  
+        self._modLevel = self._adminPlugin.config.getint("settings", "admins_level")
 		
-		self._tempban_minvotes = self.config.getint('votekick', 'tempban_minvotes')
-		self._tempban_interval = self.config.getint('votekick', 'tempban_interval')
-		self._tempban_percent = self.config.getint('votekick', 'tempban_percent')
+        self._tempban_minvotes = self.config.getint('votekick', 'tempban_minvotes')
+        self._tempban_interval = self.config.getint('votekick', 'tempban_interval')
+        self._tempban_percent = self.config.getint('votekick', 'tempban_percent')
 		
-	def start_vote(self,  data,  client):
-		m = self._adminPlugin.parseUserCmd(data)
-		if not m:
-			client.message('^7Invalid parameters')
-			return False
-		if not m[1]:
-			client.message('^7Invalid parameters, must provide a reason!')
-			return False			
-		if len(m[1]) < 3:
-			client.message("^7You should write a better motive")
+    def start_vote(self, data, client):
+        m = self._adminPlugin.parseUserCmd(data)
+        if not m:
+            client.message('^7Invalid parameters')
+            return False
+        if not m[1]:
+            client.message('^7Invalid parameters, must provide a reason!')
+            return False
+        if len(m[1]) < 3:
+            client.message("^7You should write a better motive")
 		
-		cid = m[0]
-		sclient = self._adminPlugin.findClientPrompt(cid, client)
-		if not sclient:
-			return False
+        cid = m[0]
+        sclient = self._adminPlugin.findClientPrompt(cid, client)
+        if not sclient:
+            return False
 			
-		if sclient.maxLevel >= self._modLevel:
-			client.message("You can't kick an admin!")
-			return False
+        if sclient.maxLevel >= self._modLevel:
+            client.message("You can't kick an admin!")
+            return False
 		
-		self._caller = client
-		self._victim = sclient
-		self._reason = m[1]
-		return True
+        self._caller = client
+        self._victim = sclient
+        self._reason = m[1]
+        return True
 
-	def vote_reason(self):
-		return "against ^3%s because ^3%s" % (self._victim.exactName,  self._reason)
+    def vote_reason(self):
+        return "against ^3%s because ^3%s" % (self._victim.exactName, self._reason)
 	
-	def end_vote_yes(self,  yes,  no):
-		self.console.say("^1KICKING ^3%s" %self._victim.exactName)
-		self._victim.kick("by popular vote",  None)
-		if self._tempban_interval and (yes*100.0 / no) > self._tempban_percent and yes > self._tempban_minvotes:
-			self._victim.tempban("", "Voted out", self._tempban_interval, None)
-		self._victim = None
+    def end_vote_yes(self, yes, no):
+        self.console.say("^1KICKING ^3%s" % self._victim.exactName)
+        self._victim.kick("by popular vote", None)
+        if self._tempban_interval and (yes * 100.0 / no) > self._tempban_percent and yes > self._tempban_minvotes:
+            self._victim.tempban("", "Voted out", self._tempban_interval, None)
+        self._victim = None
 
-	def end_vote_no(self,  yes,  no):
-		self.console.say("Player is ^2safe!")
-		self._victim = None
+    def end_vote_no(self, yes, no):
+        self.console.say("Player is ^2safe!")
+        self._victim = None
 
 
 class MapVote:
-	_adminPlugin = None
-	console = None
-	config = None
+    _adminPlugin = None
+    console = None
+    config = None
 
-	_mapList = []
-	_caller = None
-	_map= None
+    _mapList = []
+    _caller = None
+    _map = None
 
-	def startup(self,  adminPlugin,  console,  config):
-		"""\
+    def startup(self, adminPlugin, console, config):
+        """\
 		Initialize plugin settings
 		"""
 
-		self._adminPlugin = adminPlugin
-		self.console = console
-		self.config = config
+        self._adminPlugin = adminPlugin
+        self.console = console
+        self.config = config
 		
-		f = open(self.config.getpath('votemap',  'mapfile'))
-		for line in f:
-			self._mapList.append(line.strip())
+        f = open(self.config.getpath('votemap', 'mapfile'))
+        for line in f:
+            self._mapList.append(line.strip())
 			
 
-	def start_vote(self,  data,  client):
-		m = self._adminPlugin.parseUserCmd(data)
-		if not m:
-			client.message('^7Invalid parameters')
-			return False
-		if not m[0]:
-			client.message('^7Invalid parameters, must provide a map to vote!')
-			return False
+    def start_vote(self, data, client):
+        m = self._adminPlugin.parseUserCmd(data)
+        if not m:
+            client.message('^7Invalid parameters')
+            return False
+        if not m[0]:
+            client.message('^7Invalid parameters, must provide a map to vote!')
+            return False
 			
-		s = m[0]
-		matched = False
+        s = m[0]
+        matched = False
 		
-		for map in self._mapList:
-			if s in map:
-				self._map  = map
-				if matched:
-					client.message('^7More than one map matches the name, be more specific')
-					return False
-				matched = True
+        for map in self._mapList:
+            if s in map:
+                self._map  = map
+                if matched:
+                    client.message('^7More than one map matches the name, be more specific')
+                    return False
+                matched = True
 				
-		if not matched:
-			client.message('^7No map matched that name')
-		return matched
+        if not matched:
+            client.message('^7No map matched that name')
+        return matched
 
 
-	def vote_reason(self):
-		return "to change map to ^3%s" % (self._map)
+    def vote_reason(self):
+        return "to change map to ^3%s" % (self._map)
 
-	def end_vote_yes(self,  yes,  no):
-		self.console.say("^1Changing map to ^3%s" %self._map)
-		self.console.write("map %s" %self._map)
+    def end_vote_yes(self, yes, no):
+        self.console.say("^1Changing map to ^3%s" % self._map)
+        self.console.write("map %s" % self._map)
 
-	def end_vote_no(self,  yes,  no):
-		self.console.say("Map ^2stays!")
+    def end_vote_no(self, yes, no):
+        self.console.say("Map ^2stays!")
 	
